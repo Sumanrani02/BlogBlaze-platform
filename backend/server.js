aaacc76e78c811cb3a6e7f7a8ee6c9de276f9f16
Sumanrani02 <sumanranikohar500@gmail.com>
2025-06-07 22:39:21 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-blue-400'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
